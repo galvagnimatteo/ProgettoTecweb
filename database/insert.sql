24c74e2836ae1b1d6677650eb0b986b4cca19b61
mikigatto93 <mikigatto93@gmail.com>
2022-01-14 14:57:44 +0100

    aggiunto alt provvisorio

--- database/insert.sql
+++ database/insert.sql
@@ -1,27 +1,23 @@
-INSERT INTO Film (ID, Titolo, Genere, DataUscita, Descrizione, SrcImg, Durata)
+INSERT INTO Film (ID, Titolo, Genere, DataUscita, Descrizione, SrcImg, AltImg, Durata)
 VALUES (1,'Film 1', 'Orrore', '2021-01-01', 
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tempor turpis neque, vel feugiat justo luctus quis. Curabitur ultricies, nisl sed pretium aliquam, nisi velit dictum augue, non scelerisque sapien metus sed urna. Maecenas sem orci, interdum ac quam in, pharetra eleifend dolor. Suspendisse ultricies iaculis velit vel viverra. Proin aliquam erat at luctus posuere. In commodo odio nulla, nec sagittis diam dictum eget. Donec egestas fringilla pellentesque.
 Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam libero purus, fermentum at faucibus sed, tristique ut leo. In hac habitasse platea dictumst. Ut erat mi, sodales interdum mi imperdiet, commodo auctor purus. Sed sed ante mi. Maecenas tincidunt at dolor quis lacinia. Mauris a tellus bibendum lorem varius efficitur.', 
-'https://via.placeholder.com/316x470/FF2222/FFFFFF', 120);
+'https://via.placeholder.com/316x470/FF2222/FFFFFF','alt-immagine', 120),
 
-
-INSERT INTO Film (ID, Titolo, Genere, DataUscita, Descrizione, SrcImg, Durata)
-VALUES (2,'Film 2', 'Fantastico', '2022-03-12', 
+(2,'Film 2', 'Fantastico', '2022-03-12', 
 'Sed eu orci pellentesque, porttitor urna vitae, pellentesque ligula. Praesent sed dui quis justo fermentum vehicula. Pellentesque hendrerit nisi a aliquet semper. Sed rhoncus urna at ligula ornare, nec euismod nibh pulvinar. Phasellus sed ultrices elit, at pretium magna. Nullam purus metus, ultrices a suscipit eu, varius et diam. Suspendisse potenti. Praesent elementum lectus non metus rutrum, ac sodales leo tincidunt. Fusce lacus purus, malesuada vel tortor ac, pulvinar viverra arcu. Ut et aliquet purus, in ornare orci. Vestibulum lorem turpis, convallis vel lacinia vel, porttitor et ligula. Integer efficitur ornare cursus. Curabitur sit amet metus egestas, luctus ligula nec, lobortis nisi. Aliquam a massa egestas, facilisis sem eget, blandit ex. Ut pharetra, metus vitae ultrices bibendum, turpis velit lacinia massa, sed imperdiet urna mi in tortor. Mauris eget dolor ut ante cursus facilisis ac vitae ante.
 Etiam pulvinar et sem et egestas. Praesent condimentum augue pulvinar, elementum lorem et, dapibus eros. Vestibulum leo nisi, pellentesque non risus eget, ultricies tincidunt neque. Vivamus id consequat odio, tincidunt sollicitudin nisi. Nam laoreet pulvinar nibh, eget luctus ipsum convallis id. In a fermentum turpis. Nullam at sapien id est tristique viverra. Quisque vulputate vestibulum sapien ut molestie.'
-,'https://via.placeholder.com/316x470/FF2222/FFFFFF', 140);
+,'https://via.placeholder.com/316x470/FF2222/FFFFFF','alt-immagine', 140),
 
-INSERT INTO Film (ID, Titolo, Genere, DataUscita, Descrizione, SrcImg, Durata)
-VALUES (3,'Film 3', 'Azione', '2022-01-07', 
+(3,'Film 3', 'Azione', '2022-01-07', 
 'Etiam pulvinar et sem et egestas. Praesent condimentum augue pulvinar, elementum lorem et, dapibus eros. Vestibulum leo nisi, pellentesque non risus eget, ultricies tincidunt neque. Vivamus id consequat odio, tincidunt sollicitudin nisi. Nam laoreet pulvinar nibh, eget luctus ipsum convallis id. In a fermentum turpis. Nullam at sapien id est tristique viverra. Quisque vulputate vestibulum sapien ut molestie.
 Morbi odio mi, egestas vitae velit eu, molestie tincidunt augue. Fusce pretium ex lorem, non congue ex venenatis ut. Praesent malesuada posuere consectetur. Cras eget convallis urna. Sed ligula neque, maximus non ex vitae, scelerisque tincidunt risus. Etiam id leo in ipsum imperdiet scelerisque quis a dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur venenatis tellus augue, ornare ullamcorper augue fringilla finibus. Pellentesque porttitor mi rutrum, vestibulum diam sed, pharetra lectus.'
-,'https://via.placeholder.com/316x470/FF2222/FFFFFF', 160);
+,'https://via.placeholder.com/316x470/FF2222/FFFFFF','alt-immagine',  160),
 
-INSERT INTO Film (ID, Titolo, Genere, DataUscita, Descrizione, SrcImg, Durata)
-VALUES (4,'Film 4', 'Giallo', '2021-01-01', 
+(4,'Film 4', 'Giallo', '2021-01-01', 
 'Morbi consectetur, nulla ut gravida lobortis, purus velit commodo nunc, in rhoncus mi ex non ante. Sed sed aliquet augue. Proin fringilla vulputate arcu ac malesuada. Proin in accumsan orci, at tincidunt lectus. Etiam feugiat eros et posuere sodales. Curabitur at laoreet tortor. Sed pharetra metus viverra, iaculis ex vitae, lacinia augue. Morbi varius, ex ut dapibus ornare, arcu purus sollicitudin tortor, quis vehicula odio elit gravida ipsum. Vivamus eros tortor, bibendum eu erat at, ornare ultrices augue. Aliquam arcu metus, placerat ac vulputate gravida, sollicitudin ac nisi. Donec in tortor tellus. Cras vel sollicitudin mauris. 
 Aliquam et mattis est.Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam libero purus, fermentum at faucibus sed, tristique ut leo. In hac habitasse platea dictumst. Ut erat mi, sodales interdum mi imperdiet, commodo auctor purus. Sed sed ante mi. Maecenas tincidunt at dolor quis lacinia. Mauris a tellus bibendum lorem varius efficitur.', 
-'https://via.placeholder.com/316x470/FF2222/FFFFFF', 130);
+'https://via.placeholder.com/316x470/FF2222/FFFFFF','alt-immagine', 130);
 
 
 INSERT INTO Sala (Numero) VALUES (1), (2), (3);

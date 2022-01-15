96cc533a5758a18e753d06f692ad4cbcdca1d9c6
giovannicristellon <95687685+giovannicristellon@users.noreply.github.com>
2022-01-15 18:53:36 +0100

    migliorie menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-document.body.className = document.body.className.replace("no-js","js");
+document.body.className = document.body.className.replace("no-js", "js");
+
+var menuon = false;
+
+function togglemenu() {
+    menuon = !menuon;
+    console.log(menuon)
+    if (menuon) {
+        document.getElementById("menu").className = "openMenu";
+    }
+    else {
+        document.getElementById("menu").className =  "closedMenu";
+    }
+}

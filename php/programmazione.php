768f6cb7315d517caefe7a6f53f17ce876b0c15d
galvagnimatteo <galvagni.matteo@protonmail.com>
2022-01-23 10:35:31 +0100

    sistemata leggibilità codice

--- php/programmazione.php
+++ php/programmazione.php
@@ -1,88 +1,139 @@
 <?php
-	session_start();
+session_start();
 
-    include 'SingletonDB.php';
-    include 'mostra_errori.php';
-	include 'utils/createCastStr.php';
+include "SingletonDB.php";
+include "mostra_errori.php";
+include "utils/createCastStr.php";
 
-    $document = file_get_contents('../html/template.html');
-    $programmazione_content = file_get_contents('../html/programmazione_content.html');
+$document = file_get_contents("../html/template.html");
+$programmazione_content = file_get_contents(
+    "../html/programmazione_content.html"
+);
 
-    $db = SingletonDB::getInstance();
-    $filmsResult = $db->getConnection()->query("SELECT * FROM Film ORDER BY DataUscita DESC");
-    $db->disconnect();
+$db = SingletonDB::getInstance();
+$filmsResult = $db
+    ->getConnection()
+    ->query("SELECT * FROM Film ORDER BY DataUscita DESC");
+$db->disconnect();
 
-    $cards = "";
+$cards = "";
 
-    if(!empty($filmsResult) && $filmsResult->num_rows > 0){
+if (!empty($filmsResult) && $filmsResult->num_rows > 0) {
+    $card_prog_template = file_get_contents("../html/items/card-prog.html");
 
-        $card_prog_template = file_get_contents('../html/items/card-prog.html');
+    while ($row = $filmsResult->fetch_assoc()) {
+        $card_prog_item = $card_prog_template;
 
-        while($row = $filmsResult->fetch_assoc()) {
+        $db->connect();
+        $preparedQuery = $db
+            ->getConnection()
+            ->prepare(
+                "SELECT * FROM CastFilm JOIN Afferisce ON CastFilm.ID = Afferisce.IDCast WHERE Afferisce.IDFilm =?"
+            );
+        $preparedQuery->bind_param("i", $row["ID"]);
+        $preparedQuery->execute();
+        $castResult = $preparedQuery->get_result();
+        $db->disconnect();
 
-			$card_prog_item = $card_prog_template;
+        if (!empty($castResult) && $castResult->num_rows > 0) {
+            $cast = createCastStr($castResult);
 
-			$db->connect();
-			$preparedQuery = $db->getConnection()->prepare('SELECT * FROM CastFilm JOIN Afferisce ON CastFilm.ID = Afferisce.IDCast WHERE Afferisce.IDFilm =?');
-			$preparedQuery->bind_param("i", $row["ID"]);
-			$preparedQuery->execute();
-			$castResult = $preparedQuery->get_result();
-			$db->disconnect();
-
-			if(!empty($castResult) && $castResult->num_rows > 0){
-
-				$cast = createCastStr($castResult);
-
-				$card_prog_item = str_replace('<FILMDIRECTOR>', $cast['R'], $card_prog_item);
-	            $card_prog_item = str_replace('<FILMCAST>', $cast['A'], $card_prog_item);
-
-			}
-
-			$description = $row["Descrizione"];
-            $description = substr($description, 0, 200);
-            $description = $description . "...";
-
-            $card_prog_item = str_replace('<FILMTITLE>', $row["Titolo"], $card_prog_item);
-			$card_prog_item = str_replace('<FILMGENRE>', $row["Genere"], $card_prog_item);
-			$card_prog_item = str_replace('<FILMLENGTH>', $row["Durata"] . "'", $card_prog_item);
-			$card_prog_item = str_replace('<FILMDESCRIPTION>', $description, $card_prog_item);
-
-			$card_prog_item = str_replace('<FILM-PAGE>', "schedafilm.php?idfilm=" . $row['ID'], $card_prog_item);
-
-            $cards = $cards . $card_prog_item;
-
+            $card_prog_item = str_replace(
+                "<FILMDIRECTOR>",
+                $cast["R"],
+                $card_prog_item
+            );
+            $card_prog_item = str_replace(
+                "<FILMCAST>",
+                $cast["A"],
+                $card_prog_item
+            );
         }
 
-    }else{
+        $description = $row["Descrizione"];
+        $description = substr($description, 0, 200);
+        $description = $description . "...";
 
-        $cards = "Nessun film trovato.";
+        $card_prog_item = str_replace(
+            "<FILMTITLE>",
+            $row["Titolo"],
+            $card_prog_item
+        );
+        $card_prog_item = str_replace(
+            "<FILMGENRE>",
+            $row["Genere"],
+            $card_prog_item
+        );
+        $card_prog_item = str_replace(
+            "<FILMLENGTH>",
+            $row["Durata"] . "'",
+            $card_prog_item
+        );
+        $card_prog_item = str_replace(
+            "<FILMDESCRIPTION>",
+            $description,
+            $card_prog_item
+        );
 
+        $card_prog_item = str_replace(
+            "<FILM-PAGE>",
+            "schedafilm.php?idfilm=" . $row["ID"],
+            $card_prog_item
+        );
+
+        $cards = $cards . $card_prog_item;
     }
+} else {
+    $cards = "Nessun film trovato.";
+}
 
-    $programmazione_content = str_replace('<CARDS-PROG>', $cards, $programmazione_content);
+$programmazione_content = str_replace(
+    "<CARDS-PROG>",
+    $cards,
+    $programmazione_content
+);
 
-    $document = str_replace('<PAGETITLE>', 'Programmazione - PNG Cinema', $document);
-    $document = str_replace('<KEYWORDS>', 'programmazione, ultime uscite, ultimi film, film programmati, film in programma', $document);
-    $document = str_replace('<DESCRIPTION>', 'Pagina sulla programmazione: è possibile consultare i film e le opere in programma nelle prossime settimane.', $document);
-    $document = str_replace('<BREADCRUMB>', '<a href="home.php">Home</a> / <a href="programmazione.php">Programmazione</a>', $document);
+$document = str_replace(
+    "<PAGETITLE>",
+    "Programmazione - PNG Cinema",
+    $document
+);
+$document = str_replace(
+    "<KEYWORDS>",
+    "programmazione, ultime uscite, ultimi film, film programmati, film in programma",
+    $document
+);
+$document = str_replace(
+    "<DESCRIPTION>",
+    "Pagina sulla programmazione: è possibile consultare i film e le opere in programma nelle prossime settimane.",
+    $document
+);
+$document = str_replace(
+    "<BREADCRUMB>",
+    '<a href="home.php">Home</a> / <a href="programmazione.php">Programmazione</a>',
+    $document
+);
 
-    $document = str_replace('<JAVASCRIPT-HEAD>', '', $document);
-    $document = str_replace('<JAVASCRIPT-BODY>', '', $document);
+$document = str_replace("<JAVASCRIPT-HEAD>", "", $document);
+$document = str_replace("<JAVASCRIPT-BODY>", "", $document);
 
-    $document = str_replace('<CONTENT>', $programmazione_content, $document);
-		if(isset(	$_SESSION['a'] ))
-	{
+$document = str_replace("<CONTENT>", $programmazione_content, $document);
+if (isset($_SESSION["a"])) {
+    $document = str_replace("<LOGIN>", $_SESSION["a"], $document);
+    $document = str_replace(
+        "<LINK>",
+        "./area_utenti.php?action=getProfile",
+        $document
+    );
+} else {
+    $document = str_replace("<LOGIN>", "Login", $document);
+    $document = str_replace(
+        "<LINK>",
+        "./area_utenti.php?action=login_page",
+        $document
+    );
+}
 
-		$document=str_replace('<LOGIN>', $_SESSION['a'] , $document);
-		$document=str_replace('<LINK>', './area_utenti.php?action=getProfile' , $document);
-
-	}else{
-
-		$document=str_replace('<LOGIN>', "Login" , $document);
-		$document=str_replace('<LINK>', './area_utenti.php?action=login_page' , $document);
-
-	}
-
-    echo($document);
+echo $document;
 
 ?>
--- php/schedafilm.php
+++ php/schedafilm.php
@@ -1,108 +1,185 @@
 <?php
-	include 'SingletonDB.php';
-	include 'utils/createCastStr.php';
+include "SingletonDB.php";
+include "utils/createCastStr.php";
 
-	if (isset($_GET["idfilm"]) && is_numeric($_GET["idfilm"])) {
+if (isset($_GET["idfilm"]) && is_numeric($_GET["idfilm"])) {
+    $db = SingletonDB::getInstance();
 
-		$db = SingletonDB::getInstance();
+    $preparedQuery = $db
+        ->getConnection()
+        ->prepare("SELECT * FROM Film WHERE Film.ID=?");
+    $preparedQuery->bind_param("i", $_GET["idfilm"]);
+    $preparedQuery->execute();
+    $result1 = $preparedQuery->get_result();
 
-		$preparedQuery = $db->getConnection()->prepare('SELECT * FROM Film WHERE Film.ID=?');
-		$preparedQuery->bind_param("i", $_GET['idfilm']);
-		$preparedQuery->execute();
-		$result1 = $preparedQuery->get_result();
+    $preparedQuery2 = $db
+        ->getConnection()
+        ->prepare(
+            "SELECT * FROM CastFilm JOIN Afferisce ON CastFilm.ID = Afferisce.IDCast WHERE Afferisce.IDFilm =?"
+        );
+    $preparedQuery2->bind_param("i", $_GET["idfilm"]);
+    $preparedQuery2->execute();
+    $result2 = $preparedQuery2->get_result();
 
-		$preparedQuery2 = $db->getConnection()->prepare('SELECT * FROM CastFilm JOIN Afferisce ON CastFilm.ID = Afferisce.IDCast WHERE Afferisce.IDFilm =?');
-		$preparedQuery2->bind_param("i", $_GET['idfilm']);
-		$preparedQuery2->execute();
-		$result2 = $preparedQuery2->get_result();
+    $preparedQuery3 = $db
+        ->getConnection()
+        ->prepare(
+            "SELECT * FROM Proiezione INNER JOIN Film ON Proiezione.IDFilm = Film.ID WHERE Film.ID=? ORDER BY Data"
+        );
+    $preparedQuery3->bind_param("i", $_GET["idfilm"]);
+    $preparedQuery3->execute();
+    $result3 = $preparedQuery3->get_result();
 
-		$preparedQuery3 = $db->getConnection()->prepare('SELECT * FROM Proiezione INNER JOIN Film ON Proiezione.IDFilm = Film.ID WHERE Film.ID=? ORDER BY Data');
-		$preparedQuery3->bind_param("i", $_GET['idfilm']);
-		$preparedQuery3->execute();
-		$result3 = $preparedQuery3->get_result();
+    $db->disconnect();
 
-		$db->disconnect();
+    if (
+        !empty($result1) &&
+        $result1->num_rows > 0 &&
+        !empty($result2) &&
+        $result2->num_rows > 0
+    ) {
+        //si assume che se c'è un film ha un cast e un direttore, per questo il controllo unico
+        $dataFilm = $result1->fetch_assoc();
+        $cast = createCastStr($result2);
 
-		if (!empty($result1) && $result1->num_rows > 0 && !empty($result2) && $result2->num_rows > 0) { //si assume che se c'è un film ha un cast e un direttore, per questo il controllo unico
-			$dataFilm = $result1->fetch_assoc();
-			$cast = createCastStr($result2);
+        $document = file_get_contents("../html/template.html");
+        $schedafilm_content = file_get_contents(
+            "../html/schedafilm_content.html"
+        );
 
-			$document = file_get_contents('../html/template.html');
-			$schedafilm_content = file_get_contents('../html/schedafilm_content.html');
+        $document = str_replace(
+            "<PAGETITLE>",
+            $dataFilm["Titolo"] . " - PNG Cinema",
+            $document
+        );
+        $document = str_replace("<KEYWORDS>", $dataFilm["Titolo"], $document);
+        $document = str_replace(
+            "<DESCRIPTION>",
+            "Scheda informativa del film: " . $dataFilm["Titolo"],
+            $document
+        );
+        $document = str_replace(
+            "<BREADCRUMB>",
+            '<a href="home.php">Home</a> / <a href="programmazione.php">Programmazione</a> / <a href="schedafilm.php?idfilm=' .
+                $dataFilm["ID"] .
+                '">Scheda Film: ' .
+                $dataFilm["Titolo"] .
+                "</a>",
+            $document
+        );
 
-			$document = str_replace('<PAGETITLE>', $dataFilm['Titolo'] . ' - PNG Cinema', $document);
-			$document = str_replace('<KEYWORDS>',
-			$dataFilm['Titolo'], $document);
-			$document = str_replace('<DESCRIPTION>',
-			'Scheda informativa del film: ' . $dataFilm['Titolo'], $document);
-			$document = str_replace('<BREADCRUMB>', '<a href="home.php">Home</a> / <a href="programmazione.php">Programmazione</a> / <a href="schedafilm.php?idfilm=' . $dataFilm['ID'] . '">Scheda Film: ' . $dataFilm['Titolo'] . '</a>', $document);
+        $document = str_replace("<JAVASCRIPT-HEAD>", "", $document);
+        $document = str_replace("<JAVASCRIPT-BODY>", "", $document);
 
-			$document = str_replace('<JAVASCRIPT-HEAD>', '', $document);
-			$document = str_replace('<JAVASCRIPT-BODY>', '', $document);
+        $schedafilm_content = str_replace(
+            "<FILM-TITLE>",
+            $dataFilm["Titolo"],
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<FILM-IMG>",
+            '<img src=\'' .
+                $dataFilm["SrcImg"] .
+                ' \' alt=\'' .
+                $dataFilm["AltImg"] .
+                '\'/>',
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<FILM-GENRE>",
+            $dataFilm["Genere"],
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<RUNNING-TIME>",
+            $dataFilm["Durata"] . " min",
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<FILM-DIRECTOR>",
+            $cast["R"],
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<FILM-CAST>",
+            $cast["A"],
+            $schedafilm_content
+        );
+        $schedafilm_content = str_replace(
+            "<FILM-DESC>",
+            $dataFilm["Descrizione"],
+            $schedafilm_content
+        );
 
-			$schedafilm_content = str_replace('<FILM-TITLE>', $dataFilm['Titolo'], $schedafilm_content);
-			$schedafilm_content = str_replace('<FILM-IMG>', '<img src=\''. $dataFilm['SrcImg'] .' \' alt=\'' . $dataFilm['AltImg'] . '\'/>', $schedafilm_content);
-			$schedafilm_content = str_replace('<FILM-GENRE>', $dataFilm['Genere'] , $schedafilm_content);
-			$schedafilm_content = str_replace('<RUNNING-TIME>', $dataFilm['Durata'] . ' min' , $schedafilm_content);
-			$schedafilm_content = str_replace('<FILM-DIRECTOR>', $cast['R'], $schedafilm_content);
-			$schedafilm_content = str_replace('<FILM-CAST>',  $cast['A'], $schedafilm_content);
-			$schedafilm_content = str_replace('<FILM-DESC>',  $dataFilm["Descrizione"], $schedafilm_content);
+        if (!empty($result3) && $result3->num_rows) {
+            $filmscreeningfield_template = file_get_contents(
+                "../html/items/filmscreeningfield.html"
+            );
+            $filmscreeningfields = "";
 
-			if(!empty($result3) && $result3->num_rows){
+            while ($row = $result3->fetch_assoc()) {
+                $filmscreeningfield = $filmscreeningfield_template;
+                $filmscreeningfield = str_replace(
+                    "<DATA>",
+                    $row["Data"],
+                    $filmscreeningfield
+                );
 
-				$filmscreeningfield_template = file_get_contents('../html/items/filmscreeningfield.html');
-				$filmscreeningfields = "";
+                $db->connect();
+                $preparedQuery4 = $db
+                    ->getConnection()
+                    ->prepare(
+                        "SELECT * FROM Film INNER JOIN Proiezione ON (Film.ID = Proiezione.IDFilm) INNER JOIN Orario ON (Proiezione.ID = Orario.IDProiezione) WHERE Film.ID = ? AND Proiezione.Data = ?"
+                    );
+                $preparedQuery4->bind_param(
+                    "is",
+                    $_GET["idfilm"],
+                    $row["Data"]
+                );
+                $preparedQuery4->execute();
+                $result4 = $preparedQuery4->get_result();
+                $db->disconnect();
 
-				while($row = $result3->fetch_assoc()){
+                $hour_fields = "";
 
-					$filmscreeningfield = $filmscreeningfield_template;
-					$filmscreeningfield = str_replace('<DATA>',  $row["Data"], $filmscreeningfield);
+                while ($orarioRow = $result4->fetch_assoc()) {
+                    //si assume che se c'è una data di proiezione ci siano anche degli orari quindi nessun controllo necessario
 
-					$db->connect();
-					$preparedQuery4 = $db->getConnection()->prepare('SELECT * FROM Film INNER JOIN Proiezione ON (Film.ID = Proiezione.IDFilm) INNER JOIN Orario ON (Proiezione.ID = Orario.IDProiezione) WHERE Film.ID = ? AND Proiezione.Data = ?');
-					$preparedQuery4->bind_param("is", $_GET['idfilm'], $row["Data"]);
-					$preparedQuery4->execute();
-					$result4 = $preparedQuery4->get_result();
-					$db->disconnect();
+                    $hour_field =
+                        '<input type="submit" name="' .
+                        $orarioRow["Ora"] .
+                        '" value="' .
+                        $orarioRow["Ora"] .
+                        '">';
+                    $hour_fields .= $hour_field;
+                }
 
-					$hour_fields = "";
+                $filmscreeningfield = str_replace(
+                    "<HOURS-INPUTS>",
+                    $hour_fields,
+                    $filmscreeningfield
+                );
 
-					while($orarioRow = $result4->fetch_assoc()){ //si assume che se c'è una data di proiezione ci siano anche degli orari quindi nessun controllo necessario
+                $filmscreeningfields .= $filmscreeningfield;
+            }
 
-						$hour_field = '<input type="submit" name="' . $orarioRow["Ora"] . '" value="' . $orarioRow["Ora"] . '">';
-						$hour_fields .= $hour_field;
+            $schedafilm_content = str_replace(
+                "<SCREENING-FIELDS>",
+                $filmscreeningfields,
+                $schedafilm_content
+            );
+        } //else nessun problema, il film non ha programmazioni in corso
 
-					}
+        $document = str_replace("<CONTENT>", $schedafilm_content, $document);
 
-					$filmscreeningfield = str_replace('<HOURS-INPUTS>',  $hour_fields, $filmscreeningfield);
-
-					$filmscreeningfields .= $filmscreeningfield;
-
-				}
-
-				$schedafilm_content = str_replace('<SCREENING-FIELDS>',  $filmscreeningfields, $schedafilm_content);
-
-			} //else nessun problema, il film non ha programmazioni in corso
-
-			$document = str_replace('<CONTENT>', $schedafilm_content, $document);
-
-			echo $document;
-
-		}else{
-			header("Location: 404.php");
-			die();
-		}
-
-	}else{
-
-		header('Location: 404.php');
-		die();
-
-	}
-
-
-
-
-
+        echo $document;
+    } else {
+        header("Location: 404.php");
+        die();
+    }
+} else {
+    header("Location: 404.php");
+    die();
+}
 
 ?>

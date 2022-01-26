3a9eaa692ddc311aa666c98866d25deec4071186
galvagnimatteo <galvagni.matteo@protonmail.com>
2022-01-26 11:09:45 +0100

    aggiunti errori nei controlli lato client

--- js/controls.js
+++ js/controls.js
@@ -11,7 +11,8 @@ function registerControls(){
 
     if(!username.match(usernameRegex)){
 
-        //display error
+        document.getElementById("errorMessage").innerHTML = "L'username deve contenere solo lettere e numeri.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }
@@ -20,58 +21,40 @@ function registerControls(){
 
     if(!name.match(nameRegrex)){
 
-        //display error
+        document.getElementById("errorMessage").innerHTML = "Il nome può essere composto da sole lettere.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }
 
     if(!surname.match(nameRegrex)){
 
-        //display err;
+        document.getElementById("errorMessage").innerHTML = "Il cognome può essere composto da sole lettere.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }
 
     if(!email.includes("@")){ //non provateci neanche con espressioni regolari, l'unico modo sensato di validare una mail è mandare una mail con un codice e aspettare che l'utente lo inserisca
 
-        //err
+        document.getElementById("errorMessage").innerHTML = "Chiocciola (@) mancante nell'email.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }
 
     if(password.length < 8 || password.includes(" ")){ //tutti i caratteri vanno bene nella password tranne lo spazio, basta che ce ne siano 8. il resto della sicurezza è affidata all'utente, non ha senso imporre vincoli assurdi per un account di un cinema
 
-        //err
+        document.getElementById("errorMessage").innerHTML = "La password deve essere di almeno 8 caratteri e non può contenere spazi.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }
 
     if(password_confirm != password){
-        //err
-        return false;
-    }
 
-    return true;
-
-}
-
-function loginControls(){
-
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    let usernameRegex = /^[a-zA-Z0-9]+$/;
-
-    if(!username.match(usernameRegex)){
-
-        //display error
-        return false;
-
-    }
-
-    if(password.length < 8 || password.includes(" ")){
-
-        //err
+        document.getElementById("errorMessage").innerHTML = "La conferma password è diversa dalla password inserita.";
+        document.getElementById("errorMessage").hidden = false;
         return false;
 
     }

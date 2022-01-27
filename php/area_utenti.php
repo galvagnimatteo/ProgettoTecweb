a4f64a421cf37591755b4e5f4bc748605088b42f
tonykekw <95693535+tonykekw@users.noreply.github.com>
2022-01-27 15:21:57 +0100

    Update area_utenti.php

--- php/area_utenti.php
+++ php/area_utenti.php
@@ -3,6 +3,7 @@
 session_start();
 $now = time();
 if (isset($_SESSION["discard_after"]) && $now > $_SESSION["discard_after"]) {
+	unset($_SESSION["a"]);
     session_unset();
     session_destroy();
     session_start();

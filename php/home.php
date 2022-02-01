083d6e1591b22380f0f002748f1c4d4159347ada
giovannicristellon <95687685+giovannicristellon@users.noreply.github.com>
2022-02-01 17:51:31 +0100

    rimozione link circolari

--- php/home.php
+++ php/home.php
@@ -107,7 +107,7 @@ $home_content = str_replace(
     $home_content
 );
 $home_content = str_replace("<CARDS-HOME>", $cards, $home_content);
-$document = str_replace("/php/home.php", "#", $document);
+$document = str_replace('a href="./home.php" lang="en">Home</a>"', "home", $document);
 $document = str_replace("<CONTENT>", $home_content, $document);
 
 if (isset($_SESSION["a"])) {

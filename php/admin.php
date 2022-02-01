e005e939be264dea642905eb6c0a0147e50e0065
tonykekw <95693535+tonykekw@users.noreply.github.com>
2022-02-01 17:05:40 +0100

    Update admin.php

--- php/admin.php
+++ php/admin.php
@@ -25,6 +25,23 @@ $document = str_replace(
     $document
 );
 
+if (isset($_SESSION["a"])) {
+    $document = str_replace("<LOGIN>", $_SESSION["a"], $document);
+    $document = str_replace(
+        "<LINK>",
+        "./area_utenti.php?action=getProfile",
+        $document
+    );
+
+    $home_content = file_get_contents("../html/home_content.html");
+} else {
+    $document = str_replace("<LOGIN>", "Login", $document);
+    $document = str_replace(
+        "<LINK>",
+        "./area_utenti.php?action=login_page",
+        $document
+    );
+}
 $document = str_replace("/php/admin.php", "#", $document);
 $document = str_replace("<CONTENT>", $content, $document);
 echo $document;

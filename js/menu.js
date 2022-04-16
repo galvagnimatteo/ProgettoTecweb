11a1f24f1b82bcb0d815094b1da0e9b4bb734ffd
mikigatto93 <mikigatto93@gmail.com>
2022-04-16 11:30:07 +0200

    modifiche grafiche #136
    -sistemato menu mobile con nuova posizione e icona hamburger
    -migliorato wall of text su info e costi

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,29 @@
 var menuon = false;
+var btnChiudiMenu = document.getElementById("menubutton-chiudi");
+var btnApriMenu = document.getElementById("menubutton-apri");
 
 function togglemenu() {
     menuon = !menuon;
     if (menuon) {
         document.getElementById("menu").className = "openMenu";
+		btnChiudiMenu.style = "display: block;";
+		btnApriMenu.style = "display: none;";
     }
     else {
         document.getElementById("menu").className =  "closedMenu";
+		btnChiudiMenu.style = "display: none;";
+		btnApriMenu.style = "display: block;";
     }
 }
+
+window.addEventListener("resize", function() {
+	if(window.innerWidth > 865){
+		btnChiudiMenu.style = "display: none;";
+		btnApriMenu.style = "display: none;";	
+	}
+	
+	else{
+		btnChiudiMenu.style = "display: none;";
+		btnApriMenu.style = "display: block;";
+	}
+});

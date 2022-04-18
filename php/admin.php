052d463a4c7f3f439b24e261501004185c020c4e
galvagnimatteo <50049534+galvagnimatteo@users.noreply.github.com>
2022-04-18 16:16:44 +0200

    Revert "Revert "Admin""

--- php/admin.php
+++ php/admin.php
@@ -4,7 +4,8 @@ include "utils/pageGenerator.php";
 
 CheckSession(true,true); //verifica che la sessione sia un utente loggato ed un admin
 $content=file_get_contents("../html/admin.html");
-//GeneratePage($page,$content,$breadcrumbs,$title,$description,$keywords,$jshead,$jsbody);
-echo GeneratePage("admin",$content,"<p>admin</p>","amministazione-PNG Cinema","","","","");
+$jsbody='<script type="text/javascript" src="../js/admin.js"> </script>';
+//   GeneratePage($page,  $content,$breadcrumbs,$title,                     $description,$keywords,$jshead,$jsbody);
+echo GeneratePage("admin",$content,"admin"     ,"amministazione-PNG Cinema","",          "",       "",     $jsbody);
 
 ?>

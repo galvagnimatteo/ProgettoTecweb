a0ce30e321afa5aec4ee41a1b8acea9e6bf0b4a4
giovannicristellon <95687685+giovannicristellon@users.noreply.github.com>
2022-05-03 20:12:18 +0200

    rimosso link circolare ad admin. admin va ora accessa dalla pagina utente

--- php/utils/generaPagina.php
+++ php/utils/generaPagina.php
@@ -89,7 +89,7 @@ function GeneratePage(
 
     $output = str_replace("<MENU/>", $menu, $output);
     $admin = "";
-    if (isset($_SESSION["admin"]) && $_SESSION["admin"]) {
+    if (isset($_SESSION["admin"]) && $_SESSION["admin"]&&$page=="login") {
         $admin = "<ul><li><a href='amministrazione.php'>Amministrazione</a></li></ul>";
     }
     $output = str_replace("<ADMIN/>", $admin, $output);

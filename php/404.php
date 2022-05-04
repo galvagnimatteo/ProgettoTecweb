077866f0ac669c57ba9649c1e2aaa6c1fd7bffe5
galvagnimatteo <galvagni.matteo@protonmail.com>
2022-05-04 11:32:46 +0200

    aggiustate breadcrumbs 404

--- php/404.php
+++ php/404.php
@@ -4,12 +4,14 @@ require_once "utils/generaPagina.php";
 $document = file_get_contents("../html/template.html"); //load template
 $error_content = file_get_contents("../html/404.html"); //load content
 //GeneratePage($page,$content,$breadcrumbs,$title,$description,$keywords,$jshead,$jsbody);
+$breadcrumbs = '<a href="home.php">Home</a> / Errore 404';
+
 echo GeneratePage(
     "404",
     $error_content,
-    "",
+    $breadcrumbs,
     "404 - PNG Cinema",
-    "Errore: pagina non trovata. Digitato il link sbagliato?",
+    "Errore: pagina non trovata. Forse hai digitato il link sbagliato.",
     "",
     "",
     ''
--- php/500.php
+++ php/500.php
@@ -4,11 +4,13 @@ require_once "utils/generaPagina.php";
 $document = file_get_contents("../html/template.html"); //load template
 $error_content = file_get_contents("../html/500.html"); //load content
 //GeneratePage($page,$content,$breadcrumbs,$title,$description,$keywords,$jshead,$jsbody);
+$breadcrumbs = '<a href="home.php">Home</a> / Errore 500';
+
 echo GeneratePage(
     "error500",
     $error_content,
-    "",
-    "Error 500",
+    $breadcrumbs,
+    "500 - PNG Cinema",
     "Errore: il server ha risposto con un errore.",
     "",
     "",

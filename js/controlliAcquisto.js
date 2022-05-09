a345040c42b3008bd41f3045667c168ef25954ac
mikigatto93 <mikigatto93@gmail.com>
2022-05-09 02:03:09 +0200

    sistemato prenotazione con gli incrementi #154
    mancano i css e cambiare di conseguenza i controlli da server (toglaire i controlli sui limiti massimi e i relativi messaggi di errore)

--- js/controlliAcquisto.js
+++ js/controlliAcquisto.js
@@ -6,21 +6,12 @@ var manualRadioBtn = document.getElementById("selezManual");
 var selectNumTicketInt = document.getElementById("selectNumTicketInt");
 var selectNumTicketRed = document.getElementById("selectNumTicketRed");
 var submitButton = document.getElementById("acquistaBtn");
-var textSelectList = [
-	document.getElementById("p1fila"),
-	document.getElementById("p1numero"),
-	document.getElementById("p2fila"),
-	document.getElementById("p2numero"),
-	document.getElementById("p3fila"),
-	document.getElementById("p3numero"),
-	document.getElementById("p4fila"),
-	document.getElementById("p4numero")
-]
+var selectContainers = document.getElementsByClassName("textSelectContainer");
+var textSelectList = [];
 
 var listaPosti = [];
 var mappaPostiOccupati = {"a":[], "b":[], "c":[], "d":[], "e":[], "f":[], "g":[]};
-const MAX_POSTI_SELEZIONABILI = 4;
-const MAX_POSTI = 7*15; //tutte le sale sono uguali
+
 
 
 const messaggi = {
@@ -34,8 +25,6 @@ textRadioBtn.addEventListener("change", changeCard);
 selectNumTicketInt.addEventListener("change", calcolaPrezzoTot);
 selectNumTicketRed.addEventListener("change", calcolaPrezzoTot);
 
-selectNumTicketInt.addEventListener("change", dynamicOption);
-selectNumTicketRed.addEventListener("change", dynamicOption);
 
 selectNumTicketInt.addEventListener("change", pulisciPostiSelezionati);
 selectNumTicketRed.addEventListener("change", pulisciPostiSelezionati);
@@ -45,18 +34,18 @@ selectNumTicketRed.addEventListener("change", mostraScelteTestuali);
 
 //selectNumTicketInt.addEventListener("change", controllaInput);
 //selectNumTicketRed.addEventListener("change", controllaInput);
-
+//selectNumTicketInt.addEventListener("change", dynamicOption);
+//selectNumTicketRed.addEventListener("change", dynamicOption);
 
 //genera mappa posti occupati
 arrayPostiOccupati = document.getElementById("textSelectGroup").dataset.postiOccupati.split(",");
 for (var i = 0; i < arrayPostiOccupati.length; i++) {
 	let posto = arrayPostiOccupati[i]
-	mappaPostiOccupati[posto.charAt(0)].push(posto.substring(1));
+	mappaPostiOccupati[posto.charAt(0)].push(parseInt(posto.substring(1)));
 }
 
 
 document.getElementById("purchaseTicketForm").addEventListener("submit", function(event) {
-	event.preventDefault();
 	var tot = parseInt(selectNumTicketInt.value) + parseInt(selectNumTicketRed.value);
 	var aiuto = document.getElementsByClassName("aiutocompilaz");
 	var arrPosti;
@@ -129,43 +118,7 @@ function changeCard(event) {
 	}
 }
 
-function maxOption(max, target) {
-	max = max + 1;
-	var numChildren = target.options.length;
-	
-	if (numChildren <= max) {
-		for (var i = numChildren; i < max; i++) {
-			var opt = document.createElement("option");
-			opt.value = String(i);
-			opt.innerText = String(i);
-			target.appendChild(opt);
-		}
-	}
-	
-	else {
-		for (var i = numChildren; i > max; i--) 
-			target.removeChild(target.options[i-1]);
-		
-	}
-	
-}
 
-
-
-function dynamicOption(event) {
-	
-	var val = parseInt(event.target.value);
-	var target2 = selectNumTicketInt.getAttribute("id") == event.target.getAttribute("id") ?
-				  selectNumTicketRed : selectNumTicketInt;
-	
-	if (val == 0 && parseInt(target2.value)==0) {
-		maxOption(MAX_POSTI_SELEZIONABILI, target2);
-		maxOption(MAX_POSTI_SELEZIONABILI, event.target);
-	} else {
-		maxOption(MAX_POSTI_SELEZIONABILI - val, target2);
-		maxOption(val, event.target);
-	}
-}
 
 function calcolaPrezzoTot(event) {
 	var pInt = parseFloat(selectNumTicketInt.dataset.prezzoIntero);
@@ -213,6 +166,210 @@ function selezionePosto(event) {
 	
 	
 }
+
+
+function pulisciPostiSelezionati(event) {
+	if (!textRadioBtn.checked) {
+		listaPosti = [];
+		inputListaPosti.setAttribute("value", listaPosti.join());
+		var list = document.getElementsByClassName("seat");
+	
+		for (var i = 0; i < list.length; i++) {
+			if (list[i].getAttribute("class") == "seat libero selezionato")
+			list[i].setAttribute("class", "seat libero");
+		}
+
+	}
+}
+
+function mostraScelteTestuali(event) {
+	var tot = parseInt(selectNumTicketInt.value) + parseInt(selectNumTicketRed.value);
+	qta = textSelectList.length / 2;
+	
+	if (qta < tot && tot > 0) {
+		//ciclo perchè possono anche bypassare l'incremento dell'input e scriverlo a mano
+		for (var j = qta; j < tot; j++) {
+			generaSelettorePosti(selectContainers[j % 2], j+1);
+		}
+		
+	} else if (qta > tot && tot >= 0) {
+		for (var j = tot; j < qta; j++) {
+			var elem = textSelectList.pop();
+			var elem2 = textSelectList.pop();
+			elem.parentElement.remove();
+		}
+	}
+}
+
+function duplicati(arr) {
+    var map = {}, i, size;
+
+    for (i = 0, size = arr.length; i < size; i++){
+        if (map[arr[i]]){
+            return true;
+        }
+
+        map[arr[i]] = true;
+    }
+
+    return false;
+}
+
+
+function generaSelettorePosti(padre, num) {
+	
+	var fieldset = document.createElement("fieldset");
+	fieldset.setAttribute("class", "selezPostoFieldset");
+	var legend = document.createElement("legend");
+	legend.innerHTML = "Posto " + num + ":";
+	fieldset.appendChild(legend);
+	
+	var label1 = document.createElement("label");
+	label1.setAttribute("for", "f" + num);
+	label1.innerHTML = "Fila:";
+	fieldset.appendChild(label1);
+	
+	var sf = document.createElement("select");
+	sf.setAttribute("id", "f" + num);
+	fieldset.appendChild(sf);
+	
+	var label2 = document.createElement("label");
+	label2.setAttribute("for", "n" + num);
+	label2.innerHTML = "Numero:";
+	fieldset.appendChild(label2);
+	
+	var sn = document.createElement("select");
+	sn.setAttribute("id", "n" + num);
+	fieldset.appendChild(sn);
+	
+	padre.appendChild(fieldset);
+	
+	primaLetteraDisponibile = generaOptionFile(sf);
+	sf.addEventListener("change", function(event) {
+		generaOptionNumeri(sn, sf.value);
+	});
+	//per generarei primi valori
+	generaOptionNumeri(sn, primaLetteraDisponibile);
+	
+	textSelectList.push(sf, sn);
+	
+}
+
+
+function generaOptionFile(sf) {
+	var lettere = ["a", "b", "c", "d", "e", "f", "g"];
+	primaLettera = null;
+	for (i = 0; i < 7; i++) {
+		var lettera = lettere[i];
+		if (mappaPostiOccupati[lettera].length < 15) {
+			if (!primaLettera) {
+				primaLettera = lettera
+			}
+			var fila = document.createElement("option");
+			fila.setAttribute("value", lettera);
+			fila.innerHTML = lettera.toUpperCase();
+			sf.appendChild(fila);
+		}
+	}
+	return primaLettera
+	
+}
+
+function generaOptionNumeri(sn, lettera) {
+	
+	//rimuovi tutto
+	while (sn.firstChild) {
+    sn.removeChild(sn.lastChild);
+  }
+	
+	for (var i = 1; i < 16; i++) {
+		if (mappaPostiOccupati[lettera].indexOf(i) == -1) {
+			var numero = document.createElement("option");
+			numero.setAttribute("value", i);
+			numero.innerHTML = i;
+			sn.appendChild(numero);
+		}
+	}
+	
+}
+
+/*<fieldset class="selezPostoFieldset hide">
+	<legend>Posto 1:</legend>
+	
+	<label for="p1fila"> Fila: </label>
+	
+		<select id="p1fila" name="p1fila">
+			<option value="a">A</option>
+			<option value="b">B</option>
+			<option value="c">C</option>
+			<option value="d">D</option>
+			<option value="e">E</option>
+			<option value="f">F</option>
+			<option value="g">G</option>
+
+		</select>
+		<label for="p1numero"> Numero: </label>
+		<select id="p1numero" name="p1numero">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+			<option value="13">13</option>
+			<option value="14">14</option>
+			<option value="15">15</option>
+		</select>
+	
+	</fieldset>
+*/
+
+
+
+/*function maxOption(max, target) {
+	max = max + 1;
+	var numChildren = target.options.length;
+	
+	if (numChildren <= max) {
+		for (var i = numChildren; i < max; i++) {
+			var opt = document.createElement("option");
+			opt.value = String(i);
+			opt.innerText = String(i);
+			target.appendChild(opt);
+		}
+	}
+	
+	else {
+		for (var i = numChildren; i > max; i--) 
+			target.removeChild(target.options[i-1]);
+		
+	}
+	
+}*/
+
+
+
+/*function dynamicOption(event) {
+	
+	var val = parseInt(event.target.value);
+	var target2 = selectNumTicketInt.getAttribute("id") == event.target.getAttribute("id") ?
+				  selectNumTicketRed : selectNumTicketInt;
+	
+	if (val == 0 && parseInt(target2.value)==0) {
+		maxOption(MAX_POSTI_SELEZIONABILI, target2);
+		maxOption(MAX_POSTI_SELEZIONABILI, event.target);
+	} else {
+		maxOption(MAX_POSTI_SELEZIONABILI - val, target2);
+		maxOption(val, event.target);
+	}
+}*/
+
 /*
 function controllaInput(event) {
 	var totPosti = parseInt(selectNumTicketInt.value) + parseInt(selectNumTicketRed.value);
@@ -290,45 +447,3 @@ function controllaInput(event) {
 	
 }
 */
-
-function pulisciPostiSelezionati(event) {
-	if (!textRadioBtn.checked) {
-		listaPosti = [];
-		inputListaPosti.setAttribute("value", listaPosti.join());
-		var list = document.getElementsByClassName("seat");
-	
-		for (var i = 0; i < list.length; i++) {
-			if (list[i].getAttribute("class") == "seat libero selezionato")
-			list[i].setAttribute("class", "seat libero");
-		}
-
-	}
-}
-
-function mostraScelteTestuali(event) {
-	var tot = parseInt(selectNumTicketInt.value) + parseInt(selectNumTicketRed.value);
-	var selezPostiList = document.getElementsByClassName("selezPostoFieldset");
-	
-	
-	for (var i = 0; i < MAX_POSTI_SELEZIONABILI; i++) {
-		if (i < tot) {
-			selezPostiList[i].setAttribute("class", "selezPostoFieldset");
-		} else {
-			selezPostiList[i].setAttribute("class", "selezPostoFieldset hide");
-		}
-	}
-}
-
-function duplicati(arr) {
-    var map = {}, i, size;
-
-    for (i = 0, size = arr.length; i < size; i++){
-        if (map[arr[i]]){
-            return true;
-        }
-
-        map[arr[i]] = true;
-    }
-
-    return false;
-}
